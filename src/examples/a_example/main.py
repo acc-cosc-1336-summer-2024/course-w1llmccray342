ffb963f6b010e20827a674ff4409c64b0912bbd1
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-03 23:27:32 +0000

    Create hello world program

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,5 +1,12 @@
 import devprocess
 
+
+#"#" is used to denote a line that the 
+#Variable result stores the numbers used by devprocess module's add_numbers function
 result = devprocess.add_numbers(5, 5)
 
 print(result)
+
+#Print a builtin python function can be used to 
+
+devprocess.hello()
