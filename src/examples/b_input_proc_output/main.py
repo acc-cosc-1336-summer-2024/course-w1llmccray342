c85a4b947423a145efd68f73129e4e8ca87ec49d
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-04 00:09:58 +0000

    Added in b_input_proc functions and test cases

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,3 +1,12 @@
 import input_process_output
 
+integer = input_process_output.echo_number_whole(5)
+print(integer)
+
+floating = input_process_output.echo_number_decimal(10.5)
+print(floating)
+
+string = input_process_output.echo_string("Python")
+print(string)
+
 input_process_output.display_output()
--- tests/examples/b_input_process_output/tests_input_process_output.py
+++ tests/examples/b_input_process_output/tests_input_process_output.py
@@ -1,9 +1,19 @@
 import unittest
 
 from src.examples.b_input_proc_output.input_process_output import test_config
+from src.examples.b_input_proc_output.input_process_output import echo_number_decimal, echo_number_whole, echo_string
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_echo_number(self):
+        self.assertEqual(10, echo_number_whole(10))
+        self.assertEqual(100.5, echo_number_whole(100.5))
+
+    def test_echo_number_dec(self):
+        self.assertEqual(10.5, echo_number_decimal)
+
+    def test_echostring(self):
+        self.assertEqual("Python", echo_string("Python"))
