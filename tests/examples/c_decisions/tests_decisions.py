8ce550f56b932b8b8f0ce0f05c83429a361f958e
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-05 23:53:48 +0000

    Added lecture notes and decisions

--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -7,3 +7,6 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_number_in_a_range(self):
+        self.assertEqual(True, is_number_in_range )
+
