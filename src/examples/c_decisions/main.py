e2d012ac58f0093dc573446ff4752276216504e4
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-06 00:03:53 +0000

    Added in code for c_decisions examples.

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,2 +1,9 @@
 import decisions
 
+value1 = input("Enter a number here:")
+# Check if a value is a number
+# If number continue.
+
+result = decisions.is_number_in_range(int(value1), 1, 10)
+
+print(result)
--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.c_decisions.decisions import test_config
+from src.examples.c_decisions.decisions import test_config, is_number_in_range
 
 class Test_Config(unittest.TestCase):
 
@@ -8,5 +8,7 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(True, test_config())
 
     def test_number_in_a_range(self):
-        self.assertEqual(True, is_number_in_range )
+        self.assertEqual(True, is_number_in_range (1, 1, 10))
+        self.assertEqual(False, is_number_in_range(0, 1, 10))
+        self.assertEqual(True, is_number_in_range(5, 1, 10))
 
