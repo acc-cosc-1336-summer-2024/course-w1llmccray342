4317fb84f5afb286eed8bf91a10263d8861c06f0
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-10 23:42:55 +0000

    Removed extraneous code from tests_repetition example

--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -7,9 +7,6 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
-    def test_display_numbers(self):
-        self.assertEqual()
-
     def test_sum_of_squares(self):
         self.assertEqual(14, sum_of_squares(3))
         self.assertEqual(5, sum_of_squares(2))
