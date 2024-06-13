42ac41bcb895cd45f9e65edb0a02fff008395e68
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-13 00:23:52 +0000

    global const system

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,4 +1,20 @@
 #main program
 import void_functions, value_return_functions
 
+FEDERAL_TAX_RATE = .12
+
 void_functions.local_variables(5, 2)
+
+globalVar = "Hi"
+
+def main():
+    num1 = 2
+    num2 = 3
+
+    void_functions.local_variables(3, 2)
+    print(num1, num2)
+
+    print(globalVar)
+    print(FEDERAL_TAX_RATE)
+
+main()
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -6,3 +6,8 @@ def local_variables(num1, num2):
     sum = int(num1) + int(num2)
     print(sum)
     print("Python")
+
+def local_variables_value_return(num1, num2):
+    num3 = num1 + num2
+
+    return(num3)
