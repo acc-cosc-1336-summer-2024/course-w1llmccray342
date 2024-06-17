07b1f18d9c9eaea7076ba6bc7c2f7ba3948d4d79
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-17 23:04:50 +0000

    commit decisions corrections to main (missed >= for elif branches)

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -16,14 +16,14 @@ def get_faculty_rating(get_ratio):
 
     if get_ratio >= 0 and get_ratio <=.59:
         rating = "Unacceptable"
-    elif get_ratio > .6 and get_ratio <= .7:
+    elif get_ratio >= .6 and get_ratio <= .7:
         rating = "Needs Improvement"
-    elif get_ratio > .7 and get_ratio <= .8:
+    elif get_ratio >= .7 and get_ratio <= .8:
         rating = "Good"
-    elif get_ratio > .8 and get_ratio <= .9:
+    elif get_ratio >= .8 and get_ratio <= .9:
         rating = "Very Good"
     # Greater than equal to .9 but less than 1
-    elif get_ratio > .9 and get_ratio < 1:
+    elif get_ratio >= .9 and get_ratio < 1:
         rating = "Excellent"
     #Sanity check to make sure we're not going below the minvalue or above the maxvalue required in parameters.
     else: 
