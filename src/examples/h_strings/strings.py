1ae720ae6aeefc2d91fa4630d64201a95759e994
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-18 00:03:07 +0000

    Check strings changes into main

--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -1,7 +1,15 @@
 def test_config():
     return True
 
+#Example string
 def create_string():
     lang = "Python"
     print(lang)
-    
+
+def access_chars_in_string(string):
+    lang = "Python"
+    print(lang[0])
+    print(lang[-1])
+
+
+
