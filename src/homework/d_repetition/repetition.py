0c7371d30dde28a0b710c68df5ef4c0eb87ff151
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-18 17:04:54 +0000

    Set up factorials in repetition.py this took me an embarrassingly long time :skull:

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -3,14 +3,16 @@ def test_config():
 
 def get_factorial(num):
 
-    # Start the total at 1 so we can move from here.
     factorial_total = 0
 
-    for value in range (0, num): 
+    if num != 0:
+        for val in range (0, num):
+            factorial_total += num * val
+    else: 
+        factorial_total = 1
+    return factorial_total
 
-     factorial_total += value * factorial_total 
-     return factorial_total
-     
+
      
 
 
