672edba2753952faac8945645cb2ee63378c1f6a
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-25 00:13:46 +0000

    change lists and tuples

--- tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -4,17 +4,25 @@ from src.examples.g_lists_and_tuples.lists import test_config
 
 class Test_Config(unittest.TestCase):
 
+    # Always prefix with 'test_'
+    
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
-    def create_empty_list(self):
-        list = []
-        list.append(10)
+    def test_create_empty_list(self):
+        list_a = []
+        list_a.append(10)
 
-        self.assertEqual(True, [10] == list)
+        self.assertEqual(True, [10] == list_a)
 
-    def find_item_in_a_list(self):
-        list = [2, 4, 6, 8, 10]
+    def test_find_item_in_a_list(self):
+        list_b = [2, 4, 6, 8, 10]
+        found = 2 in list_b
 
-        self.assertEqual(True, )
+        self.assertEqual(True, found)
+
+    def test_item_not_in_list(self):
+        list_c = [2, 4, 6, 8, 10]
+
+        self.assertEqual(True, 11 not in list_c)
 
