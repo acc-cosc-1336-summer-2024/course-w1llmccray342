4fbde3dc3bdb1e288510b62dbf52d4653b3d0147
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-01 17:39:34 +0000

    refactor hamming_distance function

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -5,13 +5,23 @@ def get_hamming_distance(dna1, dna2):
     # For each different letter in the comparative sequence we should store a value of 1
 
     hamming_distance = 0
+    i = 0 
 
-    for char in dna1:
-        if char not in dna2:
+    # Iterate through each character in dna1
+    for char in dna1[i]:
+
+        # If char in dna1 matches the char in dna2
+        if char in dna1[i] == char in dna2[i]:
+            # Increment hamming distance + 1
             hamming_distance += 1
+        i += 1
+    
+    return hamming_distance
+
+
 
     return hamming_distance
 
 def  get_dna_complement(dna1, dna2):
     pass
-    # String formed by reversing the symbols of dna1
+    # String formed by reversing the symbols of dna1 and storing them into dna2
