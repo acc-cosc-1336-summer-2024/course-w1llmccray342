2fa7d934cffa2899f4a7fed760869ad3e642e257
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-01 19:13:40 +0000

    Added sanity check for dna pairs.

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,6 +1,24 @@
 #
 
+# Function to check if dna1 and dna2 are valid integers
+def check_if_valid(dna1, dna2):
+
+    if len(dna1) != len(dna2):
+        print("Invalid base pairs, Error")
+
+    
+    if len(dna1) < 0 or len(dna1) > 1000:
+        print("Invalid base pairs, Error")
+        return False
+
+    if len(dna2) < 0 or len(dna2) > 1000:
+        print("Invalid base pairs, Error")
+        return False
+
+
 def get_hamming_distance(dna1, dna2):
+
+
     # Comparing two strings against each other
     # For each different letter in the comparative sequence we should store a value of 1
     # Some way to make sure that DNA1 and DNA2 can't go above 1000 base pairs.
@@ -8,6 +26,8 @@ def get_hamming_distance(dna1, dna2):
     # Initialize a variable, hamming_distance
     hamming_distance = 0
     i = 0 
+
+    check_if_valid(dna1, dna2)
     
     # Check each base across the length of the variable dna1
     for base in range(len(dna1)):
@@ -29,5 +49,11 @@ def get_hamming_distance(dna1, dna2):
     return hamming_distance
 
 def  get_dna_complement(dna1, dna2):
+
+    check_if_valid(dna1, dna2)
     pass
     # String formed by reversing the symbols of dna1 and storing them into dna2
+
+
+
+    
