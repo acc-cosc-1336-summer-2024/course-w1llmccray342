1761c2f87d8177fe1951fdd38cb8112e63ea7dbe
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-03 23:37:40 +0000

    Append to file

--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -4,6 +4,6 @@ import files
 file_name = 'Philosophers.txt'
 
 #files.open_file(file_name)
-files.write_to_file(file_name)
+
 files.read_from_file(file_name)
 files.read_from_file_one_line_at_a_time(file_name)
