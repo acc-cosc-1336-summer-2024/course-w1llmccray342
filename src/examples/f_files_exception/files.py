1a1840befbc191920dc128f7b15ed82354d32e9f
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-03 23:22:22 +0000

    File operations

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,10 @@
-#
+#Create a file to read and write
+
+# A simple function to open the file
+def open_file(file_name):
+    file = open(file_name, 'r')
+
+    file.close()
+
+def write_to_file(param):
+    pass
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,4 @@
 #main program
+import files
+
+files.open_file('Philosophers.txt')
