49cc44bf8198482f4dfdc3a46231624b3ab8c28c
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-04 00:29:36 +0000

    langs new

--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,8 +1,8 @@
 #main program
 import files
 
-file_name = 'langs.txt'
+file_name = 'langs_new.txt'
 
 
 # files.write_2d_list_to_file(file_name)
-files.read_prog_langs_records(file_name)
+files.write_dict_into_writing(file_name)
