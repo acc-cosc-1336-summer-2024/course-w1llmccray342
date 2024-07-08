44e8e224df7a33df123b36aa7eb2e35e03ad46fd
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 23:22:10 +0000

    exceptions fix

--- src/homework/f_files_exception/exception.py
+++ src/homework/f_files_exception/exception.py
@@ -5,8 +5,9 @@ def divide_two_numbers(num1, num2):
     result = 0
 
     if num2 != 0:
-        result1 = num1/ num2
+        result = num1 / num2
     
     else:
         return "No integer is divisible by 0; ERROR"
-    return result1
+    
+    return result 
