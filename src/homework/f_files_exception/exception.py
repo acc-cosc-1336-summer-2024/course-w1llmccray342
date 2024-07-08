3fe26327337f2fec749e738222b0f3aea314e209
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 23:21:01 +0000

    commit exception.py

--- src/homework/f_files_exception/exception.py
+++ src/homework/f_files_exception/exception.py
@@ -1,1 +1,12 @@
 #
+
+def divide_two_numbers(num1, num2):
+    
+    result = 0
+
+    if num2 != 0:
+        result1 = num1/ num2
+    
+    else:
+        return "No integer is divisible by 0; ERROR"
+    return result1
--- src/homework/f_files_exception/main.py
+++ src/homework/f_files_exception/main.py
@@ -1,1 +1,7 @@
 #
+import exception
+
+result = exception.divide_two_numbers(2, 0)
+
+if (result != "No integer is divisible by 0; ERROR"):
+    print(result)
