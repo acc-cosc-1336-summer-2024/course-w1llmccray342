f439988fd0d7246fd7552896aedd69ef839a6f6b
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 16:57:43 +0000

    Added test case for get_p_distance_matrix

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,5 +1,5 @@
 import unittest
-from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance, get_p_distance_matrix
 
 class Test_Config(unittest.TestCase):
 
@@ -9,4 +9,18 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(.4, get_p_distance(list1, list2))
 
     def test_get_p_distance_matrix(self):
-        pass
+        my_val = [
+ [0.0, 0.4, 0.1, 0.1],
+ [0.4, 0.0, 0.4, 0.3],
+ [0.1, 0.4, 0.0, 0.2],
+[0.1, 0.3, 0.2, 0.0]
+]
+        
+        my_test = [
+ ['T','T','T','C','C','A','T','T','T','A'],
+ ['G','A','T','T','C','A','T','T','T','C'],
+ ['T','T','T','C','C','A','T','T','T','T'],
+ ['G','T','T','C','C','A','T','T','T','A']
+]
+
+        self.assertEqual(my_val, get_p_distance_matrix(my_test))
