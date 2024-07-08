388cc7d09eb8793d813bde1f01828b787bd495bd
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 15:51:53 +0000

    Setup initial ph functions

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,7 +1,13 @@
 #
 
+# For two strings s1 and s2 of equal length, the p-distance between them, 
+# denoted dp(s1,s2), is the proportion of corresponding symbols that differ between s1 and s2.
+
+# That is our distance between the two lists should be 
 def get_p_distance(list1, list2):
     pass
 
+# For a general matrix distance function d on n taxa s1,s2,…,sn (taxa are often represented by genetic strings), 
+# we may encode the distances between pairs of taxa via a distance matrix D in which Di,j=d(si,sj).
 def get_p_distance_matrix(list1):
     pass
