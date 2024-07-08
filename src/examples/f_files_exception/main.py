d4057fdca9bdfe631a4edaed17b5ae223e659399
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 23:27:04 +0000

    change import

--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,6 +1,6 @@
-import exception
+import exceptions
 
-result = exception.divide_two_numbers(2, 4)
+result = exceptions.divide_two_numbers(2, 4)
 
 if (result != "No integer is divisible by 0; ERROR"):
     print(result)
