10f592e1f6d6b9c2de5efef995d1701d28d014c4
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 16:16:48 +0000

    stage changes for list tests

---
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -0,0 +1,12 @@
+import unittest
+from src.homework.i_dictionaries_sets import get_p_distance, get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_p_distance(self):
+        list1 = ['T','T','T','C','C','A','T','T','T','A']
+        list2 = ['G','A','T','T','C','A','T','T','T','C']
+        self.assertEqual(.4, get_p_distance(list1, list2))
+
+    def test_get_p_distance_matrix(self):
+        pass
