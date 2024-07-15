00b1ed0f45aae27f2f1fcc4a1c0125b2e951d996
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-15 22:07:58 +0000

    Main.py

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,39 @@
-#
+from class_a import Die
+
+def create_menu():
+    menu = {1: "Roll", 2: "Exit"}
+    return menu
+
+def close_program():
+    close_program = input("Would you like to close the program? Y/N:").upper()
+
+    if close_program == "Y":
+        print("Goodbye!")
+        still_in_loop = False
+    else:
+        still_in_loop = True
+
+    return still_in_loop
+
+
+def handle_menu(menu):
+    still_in_loop = True
+
+    while still_in_loop:
+        for key, values in menu.items():
+            print(f"{key}. {values}")
+
+        user_input = int(input("Please select what you would like to do"))
+
+        if user_input == 1:
+            my_dice = Die()
+            my_dice.roll()
+        still_in_loop = close_program()
+        
+def main():
+    my_menu = create_menu()
+    
+    handle_menu(my_menu)
+
+if __name__ == "__main__":
+    main()    
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -30,9 +30,9 @@ class Test_Config(unittest.TestCase):
             
             for my_dice in my_dice_to_test:
                 # Check that the rolled_value is in the range 1, 6
-                self.assertEqual(True, Die.get_rolled_value(my_dice) in range(1, 6))
-                self.assertEqual(False, Die.get_rolled_value(my_dice) in range(7, 99))
-                print(f"DEBUG: {my_dice} is in the Range of 1-6")
+                self.assertEqual(True, Die.get_rolled_value(my_dice) in range(1, 7))
+                self.assertEqual(False, Die.get_rolled_value(my_dice) in range(8, 99))
+                print(f"DEBUG: {my_dice} is in the Range of {range(1, 7)}")
                 
                 
 
