2226c6059589edcef1effff232e8119997b962e2
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-15 22:55:18 +0000

    Check again

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -32,7 +32,7 @@ def handle_menu(menu):
         # Check to see if user input is valid
         while not user_input in [1, 2] or type(user_input) == type(int): 
             user_input = int(input("Please select what you would like to do: \n"))
-            print(f"DEBUG: {type(user_input)} and {user_input} selected and compared against {type(int)}")
+            print(f"DEBUG: {type(user_input)} and {user_input} selected and compared against {int}")
             
             if user_input not in [1, 2]:
                 print(f"Invalid option! User input must be in the designated range!")
