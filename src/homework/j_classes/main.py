17d72d743bc7d74f3671b897bb9941f11d5c4d99
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-15 22:37:01 +0000

    typechecking for user_input in handle_menu and in close_program

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -5,23 +5,34 @@ def create_menu():
     return menu
 
 def close_program():
-    close_program = input("Would you like to close the program? Y/N: \n").upper()
+
+    close_program = ""
+
+    while type(close_program) != str or close_program not in ["Y", "N"]:
+        close_program = input("Would you like to close the program? Y/N: \n").upper()
+        print("Invalid option")
 
     if close_program == "Y":
         print("Goodbye!")
         return False
     else:
         return True
-        
+            
 
 def handle_menu(menu):
     still_in_loop = True
 
     while still_in_loop:
+        user_input = 0
+        
         for key, values in menu.items():
             print(f"{key}. {values}")
-
-        user_input = int(input("Please select what you would like to do: \n"))
+        
+        # Check to see if user input is valid
+        while type(user_input) != int or user_input not in range(menu.items()): 
+            user_input = int(input("Please select what you would like to do: \n"))
+            print("Invalid option! User input must be a string value and in the designated range!")
+        
 
         if user_input == 1:
             my_dice = Die()
