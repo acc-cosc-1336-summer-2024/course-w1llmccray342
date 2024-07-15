bb59895a41dd2c81956ee7878b8e83d4ee23fda1
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-15 20:30:41 +0000

    Setup initial commits for HW6

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,18 @@
-#
+import random
+#Create a class die.
+
+class Die:
+    
+    def roll():
+        my_roll = random.randint(1, 6)
+        return my_roll
+    
+    def get_rolled_value(roll):
+        return roll
+    
+    def __str__(roll):
+        return f"The rolled value is {roll}"
+    
+    def __roll_value():
+        pass
+    
