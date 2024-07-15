02a420d6152131b7579f0a3cb7d3fa7124de3dd6
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-15 23:05:00 +0000

    Fixed an error in the try/except statement

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -36,7 +36,7 @@ def handle_menu(menu):
                 user_input = int(input("Please select what you would like to do: \n"))
                 print(f"DEBUG: {type(user_input)} and {user_input} selected and compared against {int}")
             
-                if user_input not in ["1", "2"]:
+                if user_input not in [1, 2]:
                     print(f"Invalid option! User input must be in the designated range!")
             
             except ValueError:
